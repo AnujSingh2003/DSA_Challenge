2acf05da3bd2024145b6c22b93cd9f727364a78b
Anuj Singh <anujsinghdgp123@gmail.com>
2023-07-09 03:08:20 +0530

    first commit

---
+++ majorityelement.cpp
@@ -0,0 +1,11 @@
+// leetcode 169
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       int n= nums.size();
+       int mid = n/2;
+       return nums[mid]; 
+    }
+};
