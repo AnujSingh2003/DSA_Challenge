8840d1a5479161515ab764abc39b374fa416a3bb
Anuj Singh <anujsinghdgp123@gmail.com>
2023-07-13 02:09:01 +0530

    first commit

---
+++ h-index.cpp
@@ -0,0 +1,14 @@
+// LEETCODE 274
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+    sort(citations.rbegin(), citations.rend());
+    int h = 0;
+    while (h < citations.size() && citations[h] > h) {
+        h++;
+    }
+    return h;
+}
+};
